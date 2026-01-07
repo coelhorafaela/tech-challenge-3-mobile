3a25e737dcc9342da332861cea5d05b5894c9214
Kasnix <luc.kasnix@gmail.com>
2026-01-07 19:16:25 -0300

    refactor: remove unused LogBox

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,9 @@
-import { LogBox } from "react-native";
 import { Stack } from "expo-router";
 
 import { AccountProvider } from "@/src/application/providers/account.provider";
 import { AuthProvider } from "@/src/application/providers/auth.provider";
 import { CardProvider } from "@/src/application/providers/card.provider";
 import { TransactionProvider } from "@/src/application/providers/transaction.provider";
-
-LogBox.ignoreLogs([
-  "[ERROR]",
-  "Warning:",
-]);
 
 export default function RootLayout() {
   return (
